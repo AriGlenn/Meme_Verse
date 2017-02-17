ab9b91966c9c4ef8f061bf10ebdcc0e9ef4b2c24
ariglenn <ariisawesome22@gmail.com>
2017-02-16 21:06:09 -0800

    Add meme urls into csv file

--- MAIN/JFCS_APP/spiders/meme_getURL.py
+++ MAIN/JFCS_APP/spiders/meme_getURL.py
@@ -1,5 +1,4 @@
-
-import scrapy
+import scrapy, csv
 
 
 class QuotesSpider(scrapy.Spider):
@@ -11,14 +10,19 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
 
         homes = response.css("div.post")
-
+        memeURL_Extensions = []
         x = 0
         for home in homes:
             homes1 = homes[x]
             postS_urlS_extensionS = homes1.css('a.image-list-link::attr(href)').extract()
             x += 1
-
             z = 0
             for linkExtensions in postS_urlS_extensionS:
-                linkExtensions1 = postS_urlS_extensionS[z]
+                linkExtensions1 = 'http://imgur.com' + str(postS_urlS_extensionS[0])
+                memeURL_Extensions.append(linkExtensions1)
                 print(linkExtensions1)
+
+        with open('memeURLs.csv', 'w') as out:
+            writer = csv.writer(out)
+            for memes in memeURL_Extensions:
+                writer.writerow(memes.split(","))
---
+++ MAIN/JFCS_APP/spiders/meme_imgURL.py
@@ -0,0 +1,30 @@
+import scrapy, csv
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "memes_ext"
+    start_urls = [
+        #Make list from csv file
+
+        'http://imgur.com/t/memes/',
+    ]
+
+    def parse(self, response):
+
+        homes = response.css("div.post")
+        memeURL_Extensions = []
+        x = 0
+        for home in homes:
+            homes1 = homes[x]
+            postS_urlS_extensionS = homes1.css('a.image-list-link::attr(href)').extract()
+            x += 1
+            z = 0
+            for linkExtensions in postS_urlS_extensionS:
+                linkExtensions1 = postS_urlS_extensionS[0]
+                memeURL_Extensions.append(linkExtensions1)
+                print(linkExtensions1)
+
+        with open('memeURLs.csv', 'w') as out:
+            writer = csv.writer(out)
+            for memes in memeURL_Extensions:
+                writer.writerow(memes.split(","))
