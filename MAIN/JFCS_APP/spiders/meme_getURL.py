c40cfea1ed7cae44b3ae27eb749ae6f04f6eaf4c
ariglenn <ariisawesome22@gmail.com>
2017-02-27 19:24:07 -0800

    Start to get gifs

--- MAIN/JFCS_APP/spiders/meme_getURL.py
+++ MAIN/JFCS_APP/spiders/meme_getURL.py
@@ -1,13 +1,10 @@
 import scrapy, csv
 
-
 class QuotesSpider(scrapy.Spider):
     name = "memes"
     start_urls = ['http://imgur.com/t/memes/',]
 
-
     def parse(self, response):
-
         homes = response.css("div.post")
         memeURL_Extensions = []
         x = 0
@@ -21,11 +18,9 @@ class QuotesSpider(scrapy.Spider):
                 memeURL_Extensions.append(linkExtensions1)
                 yield scrapy.Request(linkExtensions1, self.parse_results)
 
-
     def parse_results(self, response):
         memeIMG_URLs = []
         print("\n\n\n\n\n\n\n\n\n" + "URL: " + response.url)
-
         main = response.css("div.post-image")
         href = main.css('a.zoom::attr(href)').extract()
         if href:
@@ -35,12 +30,19 @@ class QuotesSpider(scrapy.Spider):
                 link = "https:" + x
                 memeIMG_URLs.append(link)
         else:
-            print('The img url is in the img src')
-            imgSrc = main.xpath('//img/@src').extract_first()
-            imgSrc =  "https:" + imgSrc
-            print(imgSrc)
-            memeIMG_URLs.append(imgSrc)
+            gif1 = response.css("div.video-container script").extract_first()
+            if gif1:
 
+                print('GIFGIFGIFGIFGIFGIFGIF: ' + str(gif1))
+                print('The img url is a gif')
+                memeIMG_URLs.append(gif1)
+
+            else:
+                print('The img url is in the img src')
+                imgSrc = main.xpath('//img/@src').extract_first()
+                imgSrc = "https:" + imgSrc
+                print(imgSrc)
+                memeIMG_URLs.append(imgSrc)
         with open('meme_img_URLs.csv', 'a') as out:
             writer = csv.writer(out)
             for links in memeIMG_URLs:
