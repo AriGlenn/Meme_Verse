1d5f1acdea747f0428c8f9ab0197512fd51a4f39
ariglenn <ariisawesome22@gmail.com>
2017-04-13 20:27:24 -0700

    Add flask port

--- MAIN/JFCS_APP/spiders/Serve.py
+++ MAIN/JFCS_APP/spiders/Serve.py
@@ -42,4 +42,5 @@ def getall():
     return jsonify(dictIDs)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
