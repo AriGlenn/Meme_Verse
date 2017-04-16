5ffa573cd7ca06fb886e3d775840f9387dc4cf49
ariglenn <ariisawesome22@gmail.com>
2017-04-16 00:29:34 -0700

    Update flask run

--- MAIN/JFCS_APP/spiders/Serve.py
+++ MAIN/JFCS_APP/spiders/Serve.py
@@ -42,5 +42,4 @@ def getall():
     return jsonify(dictIDs)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', debug=True, port=5000)
