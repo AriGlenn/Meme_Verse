861947a28c5a75629225e9e0844057cfaad98d44
ariglenn <ariisawesome22@gmail.com>
2017-04-20 12:11:54 -0700

    Add hello

--- deleteme2.py
+++ deleteme2.py
@@ -1,9 +1,6 @@
+from flask import Flask
+app = Flask(__name__)
 
-import json, requests
-
-response = requests.get('http://0.0.0.0:5000/getall')
-data = json.loads(response.text)
-name = data['5']['Name']
-print(name)
-urls = data['5']['MemeUrls'][0]
-print(urls)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
